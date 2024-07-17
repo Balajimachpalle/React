81394ea73575b8f67f6945b3ddbe46cdf68e8bb2
TukaramM <machpalletukaram109@gmail.com>
2024-07-17 20:11:48 +0530

    Hookes

--- ViteReact/src/App.jsx
+++ ViteReact/src/App.jsx
@@ -1,9 +1,30 @@
-
+import { useState } from "react";
 
 
 function App() {
+   
+  
+  let [counter, setCounter] = useState(15)
+  // let counter= 5;
+  const addvalue =()=>{
+   
+    setCounter(counter +1)
+}  
+  const removeValue = ()=>{
+    setCounter(counter -1)
+  }
+
   return (
+    <>
     <h1>Bilder Forntend vite</h1>
+    <h2>Countter value:{counter}</h2>
+    <button 
+    onClick={addvalue}>Add value: {counter}</button>
+    <br/>
+    <button
+    onClick={removeValue}>Remove Value : {counter}</button>
+
+    </>
   )
 }
 
